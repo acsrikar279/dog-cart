697bf5b9b9597de268165b76fc911e1ac8d3146d
acsrikar279 <acsrikar@gmail.com>
2019-08-23 12:15:49 +0530

    CSS added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
  
- 
 const prodsArray = [
  {id: 1, title: "Samsung Galaxy Note 10+ (Aura White, 12GB RAM, 256GB Storage) with No Cost EMI/Additional Exchange Offers", price: 79999, image: "GalaxyNote10+.jpg", brand: "samsung"},
  {id: 2, title: "Samsung Galaxy A80 (Phantom Black, 8GB RAM, 128GB Storage) with No Cost EMI/Additional Exchange Offers", price: 47999, image: "A80.jpg", brand: "samsung"},
@@ -17,8 +16,8 @@ const prodsArray = [
 ];  
 
 let cartList = [];
-let filteredList = [];
 let brandCheck = [];
+let i=0
 
 class SearchBar extends React.Component{
     constructor(props){
@@ -30,7 +29,7 @@ class SearchBar extends React.Component{
         const retrievedValue = e.target.value;
         const retrValueSmall = retrievedValue.toLowerCase();
         let filteredList = [];
-        for(let i=0; i<prodsArray.length; i++){
+        for(i=0; i<prodsArray.length; i++){
             if(prodsArray[i].title.toLowerCase().includes(retrValueSmall)){
                 filteredList.push(JSON.parse(JSON.stringify(prodsArray[i])));
             }
@@ -42,7 +41,8 @@ class SearchBar extends React.Component{
 
     render(){
         return(
-        <input type="text" name="searchBar" value={this.state.value} onChange={this.handleInput} placeholder="search here "/>);
+            <input type="text" className="searchBar" value={this.state.value} onChange={this.handleInput} placeholder="a search bar like ajax "/>
+        );
     }
 }
 
@@ -58,11 +58,11 @@ class Product extends React.Component{
  }
  render(){
    return (
-     <div className="itembox">
-         <img width="100" src={this.props.image} alt={this.props.title}/>
-         <span className="itemtitle">{this.props.title}</span>
-         <span className="itemprice">{this.props.price}</span>
-         <button onClick={this.addToCart} name={this.props.id} value={this.props.value} >Add to Cart</button>
+     <div className="grid-item">
+         <img src={this.props.image} alt={this.props.title}/>
+         <p>{this.props.title}</p>
+         <span>{this.props.price}</span>
+         <button className="addToCartButton" onClick={this.addToCart} name={this.props.id} value={this.props.value} >Add to Cart</button>
      </div>
    );
  }
@@ -93,8 +93,8 @@ class CartItem extends React.Component{
    return (
      <div>
          <img width="50" height="100" src={this.props.image} alt={this.props.title}/>
-         <span className="itemtitle">{this.props.title}</span>
-         <span className="itemprice">{this.props.price}</span>
+         <span>{this.props.title}</span>
+         <span>{this.props.price}</span>
      </div>
    );
  }
@@ -111,6 +111,8 @@ class BrandFilter extends React.Component{
         const target = event.target;        
         const name = target.name;
         const checkedStatus = target.checked;
+        var i=0;
+        let filteredList = [];
         console.log(checkedStatus);
         this.setState({
             [name]: !checkedStatus
@@ -118,7 +120,7 @@ class BrandFilter extends React.Component{
         if(checkedStatus){
             console.log("brandcheck is "+brandCheck);
             if(!brandCheck.includes(name)){
-                for(let i=0; i<prodsArray.length; i++){
+                for(i=0; i<prodsArray.length; i++){
                     if(prodsArray[i].brand === name){
                         filteredList.push(JSON.parse(JSON.stringify(prodsArray[i])));
                     }
@@ -134,7 +136,6 @@ class BrandFilter extends React.Component{
             }
             let n = filteredList.length;
             let nl = [];
-            var i = 0;
             for(i=0; i<n; i++){
                 if(!(filteredList[i].brand === name)){
                     nl.push(JSON.parse(JSON.stringify(filteredList[i])));
@@ -164,55 +165,40 @@ class BrandFilter extends React.Component{
  }
 }
 
-// class PriceFilter extends React.Component{
-//     constructor(props){
-//         super(props);
-//         this.handleInputChange = this.handleInputChange.bind(this);
-//         this.state = {minValue:'', maxValue: ''}
-//     }
-//     handleInputChange(event){
-//         const target = event.target;        
-//         const name = target.name;
-//         this.setState({[name]: target.value});
-
-//     }
-//     render(){
-//         return (
-//             <div>
-//                 <input type="text" name="minValue" value={this.state.minValue} onChange={this.handleInputChange} placeholder="Enter min price"/>
-//                 <input type="text" name="maxValue" value={this.state.maxValue} onChange={this.handleInputChange} placeholder="Enter max price"/>
-//             </div>
-//         );
-//     }
-// }
+class PriceFilter extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.state = {minValue: '' , maxValue: ''}
+    }
+    handleInputChange(event){
+        const target = event.target;        
+        const name = target.name;
+        let filteredList = [];
+        this.setState({[name]: target.value});
+    }
+    render(){
+        return (
+            <div>
+                <input type="number" name="minValue" value={this.state.minValue} onChange={this.handleInputChange} placeholder="Enter min price"/>
+                <input type="number" name="maxValue" value={this.state.maxValue} onChange={this.handleInputChange} placeholder="Enter max price"/>
+            </div>
+        );
+    }
+}
 
 class Filters extends React.Component{
     render(){
         return (
             <div>
                 <BrandFilter />
-                {/* <PriceFilter /> */}
+                <PriceFilter />
             </div>
         );
     }
 }
 
-
-// class Main extends React.Component{
-//     render(){
-//         return (
-//             <div>
-//                 <CartList cart={cartList}/>
-//                 <SearchBar />
-//                 <Filters />
-//                 <ProductsList products={prodsArray}/>
-//             </div>
-//         ); 
-//     }
-// }
-
 ReactDOM.render(<CartList cart={cartList} />, document.getElementById('cartDiv'));
 ReactDOM.render(<Filters />, document.getElementById('filtersDiv'));
 ReactDOM.render(<SearchBar />, document.getElementById('searchDiv'));
 ReactDOM.render(<ProductsList products={prodsArray} />, document.getElementById('productsDiv'));
-// ReactDOM.render(<Main />, document.getElementById('root'));
